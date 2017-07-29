c11417447c0d3b6613122a0221457dd033d6d149
Matt Przybyla <mprzybyla@smu.edu>
2017-07-29 18:21:19 -0500

    first commit

---
+++ Skill Rankings.R
@@ -0,0 +1,10 @@
+Subject <- c("computer programming", "math", "statistics", 
+               "machine learning", "domain expertise", 
+               "communication", "presentation skills", 
+               "data visualization")
+Subject
+Rank1 <- c(3, 4, 3, 1, 1, 4, 4, 3)
+Rank1
+Matt<- data.frame(matrix(col1(Subject), col2(Rank1)= Rank1, ncol= 2, nrow = 5))
+row.names.data.frame(Subject, Rank1)
+Matthew <- data.frame(Ranks = c)
